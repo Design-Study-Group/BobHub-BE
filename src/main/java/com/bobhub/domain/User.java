46d8cc87f5bcfd1d35e0871e1ea8d615b6a9b537
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2025-07-20 23:03:03 +0900

    Feat: update User

--- src/main/java/com/bobhub/domain/User.java
+++ src/main/java/com/bobhub/domain/User.java
@@ -1,4 +1,16 @@
 package com.bobhub.domain;
 
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+    private Long id;
+    private String email;
+    private String name;
+    private String picture;
 }
