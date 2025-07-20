0ebd08a141a23547299167989c36bd188e440074
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2025-07-20 23:02:35 +0900

    Feat: update MainController

--- src/main/java/com/bobhub/controller/MainController.java
+++ src/main/java/com/bobhub/controller/MainController.java
@@ -1,21 +1,11 @@
 package com.bobhub.controller;
 
-import com.bobhub.domain.User;
 import com.bobhub.mapper.UserMapper;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import java.util.List;
 
 @Controller
-public class UserController {
-
-    private final UserMapper userMapper;
-
-    public UserController(UserMapper userMapper) {
-        this.userMapper = userMapper;
-    }
-
+public class MainController {
     @GetMapping("/")
     public String main() {
         return "main";
