2c275d451d0e7439117a967b2328dd1d68f138b3
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2025-07-20 23:02:46 +0900

    Feat: add LoginController

---
+++ src/main/java/com/bobhub/controller/LoginController.java
@@ -0,0 +1,14 @@
+package com.bobhub.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String redirectToGoogle() {
+        return "redirect:/oauth2/authorization/google";
+    }
+}
+
