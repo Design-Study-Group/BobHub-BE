a45f5c74e21a04b144c6d0e2f42f6514a4d92e80
LeeInGyu <Developlee20@gmail.com>
2025-08-01 15:12:23 +0900

    Feat: 마이페이지 내 파티 기록 확인 기능

---
+++ src/main/java/com/bobhub/controller/MypageController.java
@@ -0,0 +1,36 @@
+package com.bobhub.controller;
+
+import com.bobhub.domain.Party;
+import com.bobhub.log.AllLogger;
+import com.bobhub.service.MypageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/mypage")
+@RequiredArgsConstructor
+public class MypageController {
+    private final MypageService mypageService;
+    private final AllLogger allLogger;
+
+    @GetMapping("")
+    public String mypage() {
+        return "mypage";
+    }
+
+    @GetMapping("/parties")
+    public String mypage_parties(@AuthenticationPrincipal OAuth2User oAuth2User, Model model) {
+        List<Party> parties = mypageService.getPartiesByOAuth(oAuth2User);
+
+        model.addAttribute("parties", parties);
+
+        return "mypage_parties";
+    }
+}
