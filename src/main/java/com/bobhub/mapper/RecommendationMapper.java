4070d940e2fa2ba210d3e7a48f5aecc7f22536c2
Jun <windows_2k@naver.com>
2025-08-04 17:47:44 +0900

    precommit 오류로 인해 재 커밋

--- src/main/java/com/bobhub/mapper/RecommendationMapper.java
+++ src/main/java/com/bobhub/mapper/RecommendationMapper.java
@@ -1,15 +1,18 @@
 package com.bobhub.mapper;
 
 import com.bobhub.domain.Recommendation;
-import org.apache.ibatis.annotations.Mapper;
-
 import java.util.List;
+import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface RecommendationMapper {
-    void insert(Recommendation recommendation);
-    List<Recommendation> findAll();
-    Recommendation findById(Long id);
-    void update(Recommendation recommendation);
-    void delete(Long id);
+  void insert(Recommendation recommendation);
+
+  List<Recommendation> findAll();
+
+  Recommendation findById(Long id);
+
+  void update(Recommendation recommendation);
+
+  void delete(Long id);
 }
