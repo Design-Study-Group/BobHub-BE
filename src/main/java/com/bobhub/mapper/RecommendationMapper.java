3c08e68073b906a7f4ac69e2f4b8b4d9c6986691
Jun <windows_2k@naver.com>
2025-08-05 14:00:06 +0900

    추천게시판 업로드

---
+++ src/main/java/com/bobhub/mapper/RecommendationMapper.java
@@ -0,0 +1,18 @@
+package com.bobhub.mapper;
+
+import com.bobhub.domain.Recommendation;
+import java.util.List;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface RecommendationMapper {
+    void insert(Recommendation recommendation);
+
+    List<Recommendation> findAll();
+
+    Recommendation findById(Long id);
+
+    void update(Recommendation recommendation);
+
+    void delete(Long id);
+}
