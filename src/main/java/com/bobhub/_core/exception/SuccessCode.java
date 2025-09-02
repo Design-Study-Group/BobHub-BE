12165f8c5215a2787df72d0c6c3f19781fd2c49d
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2025-09-02 14:56:41 +0900

    Fix: update SuccessCode, ErrorCode, ApiUtils

--- src/main/java/com/bobhub/_core/exception/SuccessCode.java
+++ src/main/java/com/bobhub/_core/exception/SuccessCode.java
@@ -8,11 +8,12 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum SuccessCode {
   // 200 OK
-  OK(HttpStatus.OK, "요청이 성공적으로 처리되었습니다."),
+  OK(HttpStatus.OK, "요청이 성공적으로 처리되었습니다.", "SUCCESS_001"),
 
   // 201 CREATED
-  CREATED(HttpStatus.CREATED, "리소스가 성공적으로 생성되었습니다.");
+  CREATED(HttpStatus.CREATED, "리소스가 성공적으로 생성되었습니다.", "SUCCESS_002");
 
   private final HttpStatus status;
   private final String message;
+  private final String code;
 }
