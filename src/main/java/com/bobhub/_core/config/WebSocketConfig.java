56a459e06ca7363f25cccd55021bf097c6109776
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2025-09-03 17:51:34 +0900

    Feat: add webSocket, webSocketSecurityConfig

---
+++ src/main/java/com/bobhub/_core/config/WebSocketConfig.java
@@ -0,0 +1,26 @@
+package com.bobhub._core.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+  @Override
+  public void configureMessageBroker(MessageBrokerRegistry config) {
+    config.enableSimpleBroker("/topic"); // 메시지를 구독하는 클라이언트에게 메시지를 전달할 때 사용할 prefix
+    config.setApplicationDestinationPrefixes("/app"); // 클라이언트에서 서버로 메시지를 보낼 때 사용할 prefix
+  }
+
+  @Override
+  public void registerStompEndpoints(StompEndpointRegistry registry) {
+    registry
+        .addEndpoint("/api/chatbot")
+        .setAllowedOrigins("http://localhost:5173", "https://bobhub.vercel.app")
+        .withSockJS(); // 웹소켓 연결을 위한 엔드포인트
+  }
+}
